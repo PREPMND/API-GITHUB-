9d4e0bfc2e02d9cac19246681deddadba8da4ea5
Srijan <219679461+PREPMND@users.noreply.github.com>
2025-09-08 15:14:30 +0530

    Update script.js ----PIC

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ok = document.querySelector(".ok");
+let img = document.querySelector("img");
 let result = document.querySelector(".result");
 let u = document.querySelector(".entry"); 
 let submit=document.querySelector("#submit");
@@ -6,11 +6,13 @@ let url;
 submit.addEventListener("click",function(dets){
   dets.preventDefault();
   let url = `https://api.github.com/users/${u.value.trim()}`;
-  ok.innerHTML=url;
+  
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    result.innerHTML = JSON.stringify(data.name); 
+    result.innerHTML = JSON.stringify(data.name);
+    img.classList.add("img")
+    img.setAttribute("src",data.avatar_url) 
   })
   .catch(error => {
     result.innerHTML = "INVALID USERNAME-OR SOMEISSUES";
